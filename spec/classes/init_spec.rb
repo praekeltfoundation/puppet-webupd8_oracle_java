dba3b0da654afe9b7b5fd4aa180fb2bbb69eac00
Jamie Hewland <jhewland@gmail.com>
2016-02-10 13:28:18 +0200

    Add basic tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,7 +7,54 @@ describe 'webupd8_oracle_java' do
         facts
       end
 
-      it { is_expected.to compile }
+      describe 'with default parameters' do
+        it do
+          is_expected.to compile
+
+          is_expected.to include_class('apt')
+
+          is_expected.to contain_apt__ppa('ppa:webupd8team/java')
+
+          is_expected.to contain_file('/var/cache/debconf/oracle-java8-installer.preseed')
+            .with_ensure('present')
+            .with_content('oracle-java8-installer shared/accepted-oracle-license-v1-1 select true')
+
+          is_expected.to contain_package('oracle-java8-installer')
+            .with_ensure('installed')
+            .with_responsefile('/var/cache/debconf/oracle-java8-installer.preseed')
+            .that_requires([
+              'File[/var/cache/debconf/oracle-java8-installer.preseed]',
+              'Apt::Ppa[ppa:webupd8team/java]',
+              'Class[apt::update]',
+            ])
+        end
+      end
+
+      describe 'when ensure is purged' do
+        let(:params) { { :ensure => 'purged' } }
+        it do
+          is_expected.to contain_package('oracle-java8-installer')
+            .with_ensure('purged')
+        end
+      end
+
+      describe 'when version is 9' do
+        let(:params) { { :version => '9' } }
+        it do
+          is_expected.to contain_file('/var/cache/debconf/oracle-java9-installer.preseed')
+            .with_ensure('present')
+            .with_content('oracle-java9-installer shared/accepted-oracle-license-v1-1 select true')
+
+          is_expected.to contain_package('oracle-java9-installer')
+            .with_ensure('installed')
+            .with_responsefile('/var/cache/debconf/oracle-java9-installer.preseed')
+            .that_requires([
+              'File[/var/cache/debconf/oracle-java9-installer.preseed]',
+              'Apt::Ppa[ppa:webupd8team/java]',
+              'Class[apt::update]',
+            ])
+          end
+      end
     end
   end
 end
